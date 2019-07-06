08749cd76f4aeedc555f2919f2af5d54123648ef
chenmingjian <609539125@qq.com>
2019-07-06 11:34:03 +0800

    更新代码

--- ShiShiCai/Properties/AssemblyInfo.cs
+++ ShiShiCai/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ShiShiCai")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ShiShiCai")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ShiShiCai")]
 [assembly: AssemblyProduct("ShiShiCai")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © ShiShiCai 2019")]
+[assembly: AssemblyTrademark("ShiShiCai")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]

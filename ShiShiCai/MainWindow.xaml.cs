60f6513c0262aeedaaffbabd887795d3796b4b85
chenmj201601 <609539125@qq.com>
2019-07-21 15:04:15 +0800

    主窗口设计

--- ShiShiCai/MainWindow.xaml.cs
+++ ShiShiCai/MainWindow.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ShiShiCai
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private bool mIsInited;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!mIsInited)
+            {
+                Init();
+                mIsInited = true;
+            }
+        }
+
+        void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Init()
+        {
+            WindowState = WindowState.Maximized;
         }
     }
 }

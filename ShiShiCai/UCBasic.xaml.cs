f4431e39065676ae22245f7f5fd2d66d5ff0565c
chenmj201601 <609539125@qq.com>
2019-08-11 11:33:21 +0800

    修改走势图

--- ShiShiCai/UCBasic.xaml.cs
+++ ShiShiCai/UCBasic.xaml.cs
@@ -19,6 +19,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace ShiShiCai
 {
@@ -67,6 +69,7 @@ namespace ShiShiCai
             ListBoxLotteryGroup.ItemsSource = mListGroupItems;
 
             InitLotteryItems();
+            InitLotteryPath();
         }
 
         private void InitLotteryItems()
@@ -89,10 +92,115 @@ namespace ShiShiCai
                     issueItem.D3Height = 120 * 1.0 / 10.0 * issueItem.D3 + 10;
                     issueItem.D4Height = 120 * 1.0 / 10.0 * issueItem.D4 + 10;
                     issueItem.D5Height = 120 * 1.0 / 10.0 * issueItem.D5 + 10;
-                    groupItem.Children.Add(group.ToList()[i]);
+                    groupItem.Children.Add(issueItem);
+
+                    int number = 1;
+                    var numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                    if (numberGroup == null)
+                    {
+                        numberGroup = new NumberItemGroup();
+                        numberGroup.Number = number;
+                        groupItem.NumberGroups.Add(numberGroup);
+                    }
+                    NumberItem numberItem = new NumberItem();
+                    numberItem.Number = number;
+                    numberItem.Value = issueItem.D1;
+                    numberGroup.Values.Add(numberItem);
+
+                    number = 2;
+                    numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                    if (numberGroup == null)
+                    {
+                        numberGroup = new NumberItemGroup();
+                        numberGroup.Number = number;
+                        groupItem.NumberGroups.Add(numberGroup);
+                    }
+                    numberItem = new NumberItem();
+                    numberItem.Number = number;
+                    numberItem.Value = issueItem.D2;
+                    numberGroup.Values.Add(numberItem);
+
+                    number = 3;
+                    numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                    if (numberGroup == null)
+                    {
+                        numberGroup = new NumberItemGroup();
+                        numberGroup.Number = number;
+                        groupItem.NumberGroups.Add(numberGroup);
+                    }
+                    numberItem = new NumberItem();
+                    numberItem.Number = number;
+                    numberItem.Value = issueItem.D3;
+                    numberGroup.Values.Add(numberItem);
+
+                    number = 4;
+                    numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                    if (numberGroup == null)
+                    {
+                        numberGroup = new NumberItemGroup();
+                        numberGroup.Number = number;
+                        groupItem.NumberGroups.Add(numberGroup);
+                    }
+                    numberItem = new NumberItem();
+                    numberItem.Number = number;
+                    numberItem.Value = issueItem.D4;
+                    numberGroup.Values.Add(numberItem);
+
+                    number = 5;
+                    numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                    if (numberGroup == null)
+                    {
+                        numberGroup = new NumberItemGroup();
+                        numberGroup.Number = number;
+                        groupItem.NumberGroups.Add(numberGroup);
+                    }
+                    numberItem = new NumberItem();
+                    numberItem.Number = number;
+                    numberItem.Value = issueItem.D5;
+                    numberGroup.Values.Add(numberItem);
                 }
                 mListGroupItems.Add(groupItem);
             }
+        }
+
+        private void InitLotteryPath()
+        {
+            for (int i = 0; i < mListGroupItems.Count; i++)
+            {
+                var groupItem = mListGroupItems[i];
+                int number = 1;
+                var numberGroup = groupItem.NumberGroups.FirstOrDefault(n => n.Number == number);
+                if (numberGroup != null)
+                {
+                    PathGeometry path = OptPathGemotry(numberGroup);
+                    numberGroup.Path1 = path;
+                }
+            }
+        }
+
+        private PathGeometry OptPathGemotry(NumberItemGroup numberGroup)
+        {
+            var itemWidth = 20;
+            var itemHeight = 12;
+            var firstItem = numberGroup.Values.FirstOrDefault();
+            if (firstItem != null)
+            {
+                PathSegmentCollection segments = new PathSegmentCollection();
+                double firstX = itemWidth / 2.0;
+                double firstY = firstItem.Value * 12 + itemHeight / 2.0;
+                Point first = new Point(firstX, firstY);
+                for (int j = 1; j < numberGroup.Values.Count; j++)
+                {
+                    var numberItem = numberGroup.Values[j];
+                    double x = itemWidth / 2.0 + itemWidth * j;
+                    double y = numberItem.Value * 12 + itemHeight / 2.0;
+                    Point point = new Point(x, y);
+                    segments.Add(new LineSegment { Point = point });
+                }
+                PathGeometry path = new PathGeometry { Figures = new PathFigureCollection { new PathFigure { StartPoint = first, Segments = segments } } };
+                return path;
+            }
+            return null;
         }
 
 
@@ -116,7 +224,11 @@ namespace ShiShiCai
         private class LotteryGroupItem : INotifyPropertyChanged
         {
             private int mDate;
-            private ObservableCollection<IssueItem> mChildren = new ObservableCollection<IssueItem>();
+
+            private readonly ObservableCollection<IssueItem> mChildren = new ObservableCollection<IssueItem>();
+
+            private readonly ObservableCollection<NumberItemGroup> mNumberGroups =
+                new ObservableCollection<NumberItemGroup>();
 
             public int Date
             {
@@ -127,6 +239,112 @@ namespace ShiShiCai
             public ObservableCollection<IssueItem> Children
             {
                 get { return mChildren; }
+            }
+
+            public ObservableCollection<NumberItemGroup> NumberGroups
+            {
+                get { return mNumberGroups; }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void OnPropertyChanged(string property)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region NumberItem
+
+        private class NumberItem : INotifyPropertyChanged
+        {
+            private int mNumber;
+            private int mValue;
+
+            public int Number
+            {
+                get { return mNumber; }
+                set { mNumber = value; OnPropertyChanged("Number"); }
+            }
+
+            public int Value
+            {
+                get { return mValue; }
+                set { mValue = value; OnPropertyChanged("Value"); }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void OnPropertyChanged(string property)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region NumberItemGroup
+
+        private class NumberItemGroup : INotifyPropertyChanged
+        {
+            private int mNumber;
+            private PathGeometry mPath1;
+            private PathGeometry mPath2;
+            private PathGeometry mPath3;
+            private PathGeometry mPath4;
+            private PathGeometry mPath5;
+
+            private readonly ObservableCollection<NumberItem> mValues = new ObservableCollection<NumberItem>();
+
+            public int Number
+            {
+                get { return mNumber; }
+                set { mNumber = value; OnPropertyChanged("Number"); }
+            }
+
+            public PathGeometry Path1
+            {
+                get { return mPath1; }
+                set { mPath1 = value; OnPropertyChanged("Path1"); }
+            }
+
+            public PathGeometry Path2
+            {
+                get { return mPath2; }
+                set { mPath2 = value; OnPropertyChanged("Path2"); }
+            }
+
+            public PathGeometry Path3
+            {
+                get { return mPath3; }
+                set { mPath3 = value; OnPropertyChanged("Path3"); }
+            }
+
+            public PathGeometry Path4
+            {
+                get { return mPath4; }
+                set { mPath4 = value; OnPropertyChanged("Path4"); }
+            }
+
+            public PathGeometry Path5
+            {
+                get { return mPath5; }
+                set { mPath5 = value; OnPropertyChanged("Path5"); }
+            }
+
+            public ObservableCollection<NumberItem> Values
+            {
+                get { return mValues; }
             }
 
             public event PropertyChangedEventHandler PropertyChanged;

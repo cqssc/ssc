1a445190b2b3c3bd398fccb4a3e10c11809fc9a6
chenmj201601 <609539125@qq.com>
2019-08-12 14:04:51 +0800

    添加遗漏数字字段

--- ShiShiCai/Models/IssueItem.cs
+++ ShiShiCai/Models/IssueItem.cs
@@ -56,6 +56,61 @@ namespace ShiShiCai.Models
         private double mD4Width;
         private double mD5Width;
 
+        private int mD1Lost0;
+        private int mD1Lost1;
+        private int mD1Lost2;
+        private int mD1Lost3;
+        private int mD1Lost4;
+        private int mD1Lost5;
+        private int mD1Lost6;
+        private int mD1Lost7;
+        private int mD1Lost8;
+        private int mD1Lost9;
+
+        private int mD2Lost0;
+        private int mD2Lost1;
+        private int mD2Lost2;
+        private int mD2Lost3;
+        private int mD2Lost4;
+        private int mD2Lost5;
+        private int mD2Lost6;
+        private int mD2Lost7;
+        private int mD2Lost8;
+        private int mD2Lost9;
+
+        private int mD3Lost0;
+        private int mD3Lost1;
+        private int mD3Lost2;
+        private int mD3Lost3;
+        private int mD3Lost4;
+        private int mD3Lost5;
+        private int mD3Lost6;
+        private int mD3Lost7;
+        private int mD3Lost8;
+        private int mD3Lost9;
+
+        private int mD4Lost0;
+        private int mD4Lost1;
+        private int mD4Lost2;
+        private int mD4Lost3;
+        private int mD4Lost4;
+        private int mD4Lost5;
+        private int mD4Lost6;
+        private int mD4Lost7;
+        private int mD4Lost8;
+        private int mD4Lost9;
+
+        private int mD5Lost0;
+        private int mD5Lost1;
+        private int mD5Lost2;
+        private int mD5Lost3;
+        private int mD5Lost4;
+        private int mD5Lost5;
+        private int mD5Lost6;
+        private int mD5Lost7;
+        private int mD5Lost8;
+        private int mD5Lost9;
+
 
         public string Serial
         {
@@ -230,6 +285,326 @@ namespace ShiShiCai.Models
             get { return mD5Width; }
             set { mD5Width = value; OnPropertyChanged("D5Width"); }
         }
+
+        #region Lost
+
+
+        #region D1
+
+        public int D1Lost0
+        {
+            get { return mD1Lost0; }
+            set { mD1Lost0 = value; OnPropertyChanged("D1Lost0"); }
+        }
+
+        public int D1Lost1
+        {
+            get { return mD1Lost1; }
+            set { mD1Lost1 = value; OnPropertyChanged("D1Lost1"); }
+        }
+
+        public int D1Lost2
+        {
+            get { return mD1Lost2; }
+            set { mD1Lost2 = value; OnPropertyChanged("D1Lost2"); }
+        }
+
+        public int D1Lost3
+        {
+            get { return mD1Lost3; }
+            set { mD1Lost3 = value; OnPropertyChanged("D1Lost3"); }
+        }
+
+        public int D1Lost4
+        {
+            get { return mD1Lost4; }
+            set { mD1Lost4 = value; OnPropertyChanged("D1Lost4"); }
+        }
+
+        public int D1Lost5
+        {
+            get { return mD1Lost5; }
+            set { mD1Lost5 = value; OnPropertyChanged("D1Lost5"); }
+        }
+
+        public int D1Lost6
+        {
+            get { return mD1Lost6; }
+            set { mD1Lost6 = value; OnPropertyChanged("D1Lost6"); }
+        }
+
+        public int D1Lost7
+        {
+            get { return mD1Lost7; }
+            set { mD1Lost7 = value; OnPropertyChanged("D1Lost7"); }
+        }
+        public int D1Lost8
+        {
+            get { return mD1Lost8; }
+            set { mD1Lost8 = value; OnPropertyChanged("D1Lost8"); }
+        }
+        public int D1Lost9
+        {
+            get { return mD1Lost9; }
+            set { mD1Lost9 = value; OnPropertyChanged("D1Lost9"); }
+        }
+
+        #endregion
+
+
+        #region D2
+
+        public int D2Lost0
+        {
+            get { return mD2Lost0; }
+            set { mD2Lost0 = value; OnPropertyChanged("D2Lost0"); }
+        }
+
+        public int D2Lost1
+        {
+            get { return mD2Lost1; }
+            set { mD2Lost1 = value; OnPropertyChanged("D2Lost1"); }
+        }
+
+        public int D2Lost2
+        {
+            get { return mD2Lost2; }
+            set { mD2Lost2 = value; OnPropertyChanged("D2Lost2"); }
+        }
+
+        public int D2Lost3
+        {
+            get { return mD2Lost3; }
+            set { mD2Lost3 = value; OnPropertyChanged("D2Lost3"); }
+        }
+
+        public int D2Lost4
+        {
+            get { return mD2Lost4; }
+            set { mD2Lost4 = value; OnPropertyChanged("D2Lost4"); }
+        }
+
+        public int D2Lost5
+        {
+            get { return mD2Lost5; }
+            set { mD2Lost5 = value; OnPropertyChanged("D2Lost5"); }
+        }
+
+        public int D2Lost6
+        {
+            get { return mD2Lost6; }
+            set { mD2Lost6 = value; OnPropertyChanged("D2Lost6"); }
+        }
+
+        public int D2Lost7
+        {
+            get { return mD2Lost7; }
+            set { mD2Lost7 = value; OnPropertyChanged("D2Lost7"); }
+        }
+        public int D2Lost8
+        {
+            get { return mD2Lost8; }
+            set { mD2Lost8 = value; OnPropertyChanged("D2Lost8"); }
+        }
+        public int D2Lost9
+        {
+            get { return mD2Lost9; }
+            set { mD2Lost9 = value; OnPropertyChanged("D2Lost9"); }
+        }
+
+        #endregion
+
+
+        #region D3
+
+        public int D3Lost0
+        {
+            get { return mD3Lost0; }
+            set { mD3Lost0 = value; OnPropertyChanged("D3Lost0"); }
+        }
+
+        public int D3Lost1
+        {
+            get { return mD3Lost1; }
+            set { mD3Lost1 = value; OnPropertyChanged("D3Lost1"); }
+        }
+
+        public int D3Lost2
+        {
+            get { return mD3Lost2; }
+            set { mD3Lost2 = value; OnPropertyChanged("D3Lost2"); }
+        }
+
+        public int D3Lost3
+        {
+            get { return mD3Lost3; }
+            set { mD3Lost3 = value; OnPropertyChanged("D3Lost3"); }
+        }
+
+        public int D3Lost4
+        {
+            get { return mD3Lost4; }
+            set { mD3Lost4 = value; OnPropertyChanged("D3Lost4"); }
+        }
+
+        public int D3Lost5
+        {
+            get { return mD3Lost5; }
+            set { mD3Lost5 = value; OnPropertyChanged("D3Lost5"); }
+        }
+
+        public int D3Lost6
+        {
+            get { return mD3Lost6; }
+            set { mD3Lost6 = value; OnPropertyChanged("D3Lost6"); }
+        }
+
+        public int D3Lost7
+        {
+            get { return mD3Lost7; }
+            set { mD3Lost7 = value; OnPropertyChanged("D3Lost7"); }
+        }
+        public int D3Lost8
+        {
+            get { return mD3Lost8; }
+            set { mD3Lost8 = value; OnPropertyChanged("D3Lost8"); }
+        }
+        public int D3Lost9
+        {
+            get { return mD3Lost9; }
+            set { mD3Lost9 = value; OnPropertyChanged("D3Lost9"); }
+        }
+
+        #endregion
+
+
+        #region D4
+
+        public int D4Lost0
+        {
+            get { return mD4Lost0; }
+            set { mD4Lost0 = value; OnPropertyChanged("D4Lost0"); }
+        }
+
+        public int D4Lost1
+        {
+            get { return mD4Lost1; }
+            set { mD4Lost1 = value; OnPropertyChanged("D4Lost1"); }
+        }
+
+        public int D4Lost2
+        {
+            get { return mD4Lost2; }
+            set { mD4Lost2 = value; OnPropertyChanged("D4Lost2"); }
+        }
+
+        public int D4Lost3
+        {
+            get { return mD4Lost3; }
+            set { mD4Lost3 = value; OnPropertyChanged("D4Lost3"); }
+        }
+
+        public int D4Lost4
+        {
+            get { return mD4Lost4; }
+            set { mD4Lost4 = value; OnPropertyChanged("D4Lost4"); }
+        }
+
+        public int D4Lost5
+        {
+            get { return mD4Lost5; }
+            set { mD4Lost5 = value; OnPropertyChanged("D4Lost5"); }
+        }
+
+        public int D4Lost6
+        {
+            get { return mD4Lost6; }
+            set { mD4Lost6 = value; OnPropertyChanged("D4Lost6"); }
+        }
+
+        public int D4Lost7
+        {
+            get { return mD4Lost7; }
+            set { mD4Lost7 = value; OnPropertyChanged("D4Lost7"); }
+        }
+        public int D4Lost8
+        {
+            get { return mD4Lost8; }
+            set { mD4Lost8 = value; OnPropertyChanged("D4Lost8"); }
+        }
+        public int D4Lost9
+        {
+            get { return mD4Lost9; }
+            set { mD4Lost9 = value; OnPropertyChanged("D4Lost9"); }
+        }
+
+        #endregion
+
+
+        #region D5
+
+        public int D5Lost0
+        {
+            get { return mD5Lost0; }
+            set { mD5Lost0 = value; OnPropertyChanged("D5Lost0"); }
+        }
+
+        public int D5Lost1
+        {
+            get { return mD5Lost1; }
+            set { mD5Lost1 = value; OnPropertyChanged("D5Lost1"); }
+        }
+
+        public int D5Lost2
+        {
+            get { return mD5Lost2; }
+            set { mD5Lost2 = value; OnPropertyChanged("D5Lost2"); }
+        }
+
+        public int D5Lost3
+        {
+            get { return mD5Lost3; }
+            set { mD5Lost3 = value; OnPropertyChanged("D5Lost3"); }
+        }
+
+        public int D5Lost4
+        {
+            get { return mD5Lost4; }
+            set { mD5Lost4 = value; OnPropertyChanged("D5Lost4"); }
+        }
+
+        public int D5Lost5
+        {
+            get { return mD5Lost5; }
+            set { mD5Lost5 = value; OnPropertyChanged("D5Lost5"); }
+        }
+
+        public int D5Lost6
+        {
+            get { return mD5Lost6; }
+            set { mD5Lost6 = value; OnPropertyChanged("D5Lost6"); }
+        }
+
+        public int D5Lost7
+        {
+            get { return mD5Lost7; }
+            set { mD5Lost7 = value; OnPropertyChanged("D5Lost7"); }
+        }
+        public int D5Lost8
+        {
+            get { return mD5Lost8; }
+            set { mD5Lost8 = value; OnPropertyChanged("D5Lost8"); }
+        }
+        public int D5Lost9
+        {
+            get { return mD5Lost9; }
+            set { mD5Lost9 = value; OnPropertyChanged("D5Lost9"); }
+        }
+
+        #endregion
+       
+
+        #endregion
 
 
         public event PropertyChangedEventHandler PropertyChanged;

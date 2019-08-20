b64fef50aa5ee65aba70f5e21372e7f91a9afc07
chenmj201601 <609539125@qq.com>
2019-08-20 10:15:39 +0800

    添加遗漏数据显示

--- ShiShiCai/UserControls/UCBasic.xaml.cs
+++ ShiShiCai/UserControls/UCBasic.xaml.cs
@@ -15,12 +15,16 @@
 //
 //======================================================================
 
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using ShiShiCai.Common;
 using ShiShiCai.Models;
 
 namespace ShiShiCai.UserControls
@@ -204,6 +208,7 @@ namespace ShiShiCai.UserControls
 
         #region Members
 
+        private readonly List<IssueItem> mListLostData = new List<IssueItem>();
         private readonly ObservableCollection<IssueDateItem> mListGroupItems = new ObservableCollection<IssueDateItem>();
         private readonly ObservableCollection<PositionItem> mListPositionItems = new ObservableCollection<PositionItem>();
         private readonly ObservableCollection<IssueItem> mListIssueItems = new ObservableCollection<IssueItem>();
@@ -247,11 +252,111 @@ namespace ShiShiCai.UserControls
 
         private void Init()
         {
+            LoadLostData();
             InitPositionItems();
             InitIssueItems();
             InitIssueDateItems();
             InitIssuePaths();
             InitPath();
+        }
+
+        private void LoadLostData()
+        {
+            mListLostData.Clear();
+            if (PageParent == null) { return; }
+            var issueItems = PageParent.ListIssueItems;
+            if (issueItems == null) { return; }
+            var systemConfig = PageParent.SystemConfig;
+            if (systemConfig == null) { return; }
+            var db = systemConfig.Database;
+            if (db == null) { return; }
+            string strConn = db.GetConnectionString();
+            var first = issueItems.FirstOrDefault();
+            if (first == null) { return; }
+            long begin = long.Parse(first.Serial);
+            long end = long.Parse(first.Serial);
+            for (int i = 0; i < issueItems.Count; i++)
+            {
+                var item = issueItems[i];
+                long serial = long.Parse(item.Serial);
+                begin = Math.Min(serial, begin);
+                end = Math.Max(serial, end);
+            }
+            string strSql = string.Format(
+                "SELECT * FROM T_102_19 WHERE C001 >= {0} AND C001 <= {1} ORDER BY C001 DESC", begin, end);
+            OperationReturn optReturn = MssqlOperation.GetDataSet(strConn, strSql);
+            if (!optReturn.Result)
+            {
+                ShowException(string.Format("load lost data fail. [{0}]{1}", optReturn.Code, optReturn.Message));
+                return;
+            }
+            DataSet objDataSet = optReturn.Data as DataSet;
+            if (objDataSet == null) { return; }
+            for (int i = 0; i < objDataSet.Tables[0].Rows.Count; i++)
+            {
+                DataRow dr = objDataSet.Tables[0].Rows[i];
+                IssueItem item = new IssueItem();
+                item.Serial = dr["C001"].ToString();
+                item.Date = Convert.ToInt32(dr["C002"]);
+                item.Number = Convert.ToInt32(dr["C003"]);
+
+                item.D5Lost0 = Convert.ToInt32(dr["C050"]);
+                item.D5Lost1 = Convert.ToInt32(dr["C051"]);
+                item.D5Lost2 = Convert.ToInt32(dr["C052"]);
+                item.D5Lost3 = Convert.ToInt32(dr["C053"]);
+                item.D5Lost4 = Convert.ToInt32(dr["C054"]);
+                item.D5Lost5 = Convert.ToInt32(dr["C055"]);
+                item.D5Lost6 = Convert.ToInt32(dr["C056"]);
+                item.D5Lost7 = Convert.ToInt32(dr["C057"]);
+                item.D5Lost8 = Convert.ToInt32(dr["C058"]);
+                item.D5Lost9 = Convert.ToInt32(dr["C059"]);
+
+                item.D4Lost0 = Convert.ToInt32(dr["C040"]);
+                item.D4Lost1 = Convert.ToInt32(dr["C041"]);
+                item.D4Lost2 = Convert.ToInt32(dr["C042"]);
+                item.D4Lost3 = Convert.ToInt32(dr["C043"]);
+                item.D4Lost4 = Convert.ToInt32(dr["C044"]);
+                item.D4Lost5 = Convert.ToInt32(dr["C045"]);
+                item.D4Lost6 = Convert.ToInt32(dr["C046"]);
+                item.D4Lost7 = Convert.ToInt32(dr["C047"]);
+                item.D4Lost8 = Convert.ToInt32(dr["C048"]);
+                item.D4Lost9 = Convert.ToInt32(dr["C049"]);
+
+                item.D3Lost0 = Convert.ToInt32(dr["C030"]);
+                item.D3Lost1 = Convert.ToInt32(dr["C031"]);
+                item.D3Lost2 = Convert.ToInt32(dr["C032"]);
+                item.D3Lost3 = Convert.ToInt32(dr["C033"]);
+                item.D3Lost4 = Convert.ToInt32(dr["C034"]);
+                item.D3Lost5 = Convert.ToInt32(dr["C035"]);
+                item.D3Lost6 = Convert.ToInt32(dr["C036"]);
+                item.D3Lost7 = Convert.ToInt32(dr["C037"]);
+                item.D3Lost8 = Convert.ToInt32(dr["C038"]);
+                item.D3Lost9 = Convert.ToInt32(dr["C039"]);
+
+                item.D2Lost0 = Convert.ToInt32(dr["C020"]);
+                item.D2Lost1 = Convert.ToInt32(dr["C021"]);
+                item.D2Lost2 = Convert.ToInt32(dr["C022"]);
+                item.D2Lost3 = Convert.ToInt32(dr["C023"]);
+                item.D2Lost4 = Convert.ToInt32(dr["C024"]);
+                item.D2Lost5 = Convert.ToInt32(dr["C025"]);
+                item.D2Lost6 = Convert.ToInt32(dr["C026"]);
+                item.D2Lost7 = Convert.ToInt32(dr["C027"]);
+                item.D2Lost8 = Convert.ToInt32(dr["C028"]);
+                item.D2Lost9 = Convert.ToInt32(dr["C029"]);
+
+                item.D1Lost0 = Convert.ToInt32(dr["C010"]);
+                item.D1Lost1 = Convert.ToInt32(dr["C011"]);
+                item.D1Lost2 = Convert.ToInt32(dr["C012"]);
+                item.D1Lost3 = Convert.ToInt32(dr["C013"]);
+                item.D1Lost4 = Convert.ToInt32(dr["C014"]);
+                item.D1Lost5 = Convert.ToInt32(dr["C015"]);
+                item.D1Lost6 = Convert.ToInt32(dr["C016"]);
+                item.D1Lost7 = Convert.ToInt32(dr["C017"]);
+                item.D1Lost8 = Convert.ToInt32(dr["C018"]);
+                item.D1Lost9 = Convert.ToInt32(dr["C019"]);
+
+                mListLostData.Add(item);
+            }
         }
 
         private void InitPositionItems()
@@ -297,6 +402,65 @@ namespace ShiShiCai.UserControls
             if (items == null) { return; }
             foreach (var item in items)
             {
+                var data = mListLostData.FirstOrDefault(l => l.Serial == item.Serial);
+                if (data != null)
+                {
+                    item.D1Lost0 = data.D1Lost0;
+                    item.D1Lost1 = data.D1Lost1;
+                    item.D1Lost2 = data.D1Lost2;
+                    item.D1Lost3 = data.D1Lost3;
+                    item.D1Lost4 = data.D1Lost4;
+                    item.D1Lost5 = data.D1Lost5;
+                    item.D1Lost6 = data.D1Lost6;
+                    item.D1Lost7 = data.D1Lost7;
+                    item.D1Lost8 = data.D1Lost8;
+                    item.D1Lost9 = data.D1Lost9;
+
+                    item.D2Lost0 = data.D2Lost0;
+                    item.D2Lost1 = data.D2Lost1;
+                    item.D2Lost2 = data.D2Lost2;
+                    item.D2Lost3 = data.D2Lost3;
+                    item.D2Lost4 = data.D2Lost4;
+                    item.D2Lost5 = data.D2Lost5;
+                    item.D2Lost6 = data.D2Lost6;
+                    item.D2Lost7 = data.D2Lost7;
+                    item.D2Lost8 = data.D2Lost8;
+                    item.D2Lost9 = data.D2Lost9;
+
+                    item.D3Lost0 = data.D3Lost0;
+                    item.D3Lost1 = data.D3Lost1;
+                    item.D3Lost2 = data.D3Lost2;
+                    item.D3Lost3 = data.D3Lost3;
+                    item.D3Lost4 = data.D3Lost4;
+                    item.D3Lost5 = data.D3Lost5;
+                    item.D3Lost6 = data.D3Lost6;
+                    item.D3Lost7 = data.D3Lost7;
+                    item.D3Lost8 = data.D3Lost8;
+                    item.D3Lost9 = data.D3Lost9;
+
+                    item.D4Lost0 = data.D4Lost0;
+                    item.D4Lost1 = data.D4Lost1;
+                    item.D4Lost2 = data.D4Lost2;
+                    item.D4Lost3 = data.D4Lost3;
+                    item.D4Lost4 = data.D4Lost4;
+                    item.D4Lost5 = data.D4Lost5;
+                    item.D4Lost6 = data.D4Lost6;
+                    item.D4Lost7 = data.D4Lost7;
+                    item.D4Lost8 = data.D4Lost8;
+                    item.D4Lost9 = data.D4Lost9;
+
+                    item.D5Lost0 = data.D5Lost0;
+                    item.D5Lost1 = data.D5Lost1;
+                    item.D5Lost2 = data.D5Lost2;
+                    item.D5Lost3 = data.D5Lost3;
+                    item.D5Lost4 = data.D5Lost4;
+                    item.D5Lost5 = data.D5Lost5;
+                    item.D5Lost6 = data.D5Lost6;
+                    item.D5Lost7 = data.D5Lost7;
+                    item.D5Lost8 = data.D5Lost8;
+                    item.D5Lost9 = data.D5Lost9;
+                }
+
                 item.D1Width = item.D1 * 20.0 + 20;
                 item.D2Width = item.D2 * 20.0 + 20;
                 item.D3Width = item.D3 * 20.0 + 20;
